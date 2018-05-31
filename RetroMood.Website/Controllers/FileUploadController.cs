969b04de798efb87f55a8c8d7eb5e61851a520fb
Giorgio Minardi <george2giga@gmail.com>
2018-05-31 14:39:24 +0100

    webapi controller draft

---
+++ RetroMood.Website/Controllers/FileUploadController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RetroMood.Website.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/FileUpload")]
+    public class FileUploadController : Controller
+    {
+        // GET: api/FileUpload
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/FileUpload/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+        
+        // POST: api/FileUpload
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+        
+        // PUT: api/FileUpload/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
