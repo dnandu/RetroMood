f4f98044633165859b72d12f9714bca5fec5718a
Giorgio Minardi <george2giga@gmail.com>
2018-05-31 15:41:54 +0100

    draft csvreader

--- RetroMood.Website/Utils/CsvParser.cs
+++ RetroMood.Website/Utils/CsvParser.cs
@@ -1,11 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using CsvHelper;
 
 namespace RetroMood.Website.Utils
 {
     public class CsvParser
     {
+        public void ReadFile(string fullFilePath)
+        {
+            TextReader textReader = File.OpenText(fullFilePath);
+            var csvReader = new CsvReader(textReader);
+            // we skip the header 
+            csvReader.Read();
+            csvReader.ReadHeader();
+
+        }
     }
 }
